36cd0548c23d182ccc482e27f1505883f25291b7
Richard McGhee <richardjmcghee@yahoo.com>
2021-07-16 19:14:03 -0400

    Created base app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Got a fresh new start!
         </p>
         <a
           className="App-link"
